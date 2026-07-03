41f14cfe5d2da351ac3c8cf01cd0119a2313ebbe
Alex Comery <alexcomery@gmail.com>
2026-07-03 07:32:59 -0700

    Add G.O.A.T. comp for 2351 N Cambria Ave (avg of 3)

--- gen_html3.py
+++ gen_html3.py
@@ -325,6 +325,7 @@ COMPS = {
     "2351 N Cambria Ave, Landers, CA 92285": [
         {"name":"Casa Verde · Poolside Stargazing Paradise","url":"https://www.airbnb.com/rooms/1421212089738528659","annual_rev":88300,"adr":322,"occ":72.1},
         {"name":"Escondite By Homestead Modern","url":"https://www.airbnb.com/rooms/1010358724851399161","annual_rev":103000,"adr":417,"occ":58.1},
+        {"name":"The G.O.A.T. | Pool & Spa | 5-Acres | No Neighbors","url":"https://www.airbnb.com/rooms/1052620076857830533","annual_rev":107000,"adr":363,"occ":67.7},
     ],
 }
 
