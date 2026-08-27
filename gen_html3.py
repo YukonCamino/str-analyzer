affbfc9a16fcc11ff1859cf7333bc14cfafeaa9d
Yukon Camino <46504508+YukonCamino@users.noreply.github.com>
2026-08-26 22:03:07 -0700

    Add 5188 Godwin Rd, Twentynine Palms to Desert tab

--- gen_html3.py
+++ gen_html3.py
@@ -59,6 +59,7 @@ LATLNG = {
     "6888 Sunnyhill Rd, Joshua Tree, CA 92252": (34.127972, -116.328287),
     "4883 Avenida La Candela, Joshua Tree, CA 92252": (34.164303, -116.314651),
     "56674 Cone Blvd, Landers, CA 92285": (34.287687, -116.426631),
+    "5188 Godwin Rd, Twentynine Palms, CA 92277": (34.157590, -115.933533),
 }
 
 def fmt_money(v):
@@ -345,6 +346,7 @@ money_tab = [
     {"address":"2351 N Cambria Ave, Landers, CA 92285","region":"Landers","price":499000,"beds":3,"baths":2,"sqft":1467,"dom":6,"img_src":b64("12"),"zillow_link":"https://www.zillow.com/homedetails/2351-N-Cambria-Ave-Landers-CA-92285/463469847_zpid/","fin":compute(499000,109600,3043.90,149700,1467,has_pool=True)},
     {"address":"4883 Avenida La Candela, Joshua Tree, CA 92252","region":"Joshua Tree","price":425000,"beds":3,"baths":2,"sqft":1392,"dom":377,"img_src":"photos/4883-avenida-la-candela.jpg","rev_source":"Adj. 6-mo actuals","fin":compute(425000,42000,2622.28,127500,1392,furnished=True)},
     {"address":"56674 Cone Blvd, Landers, CA 92285","region":"Landers","price":265000,"original_price":285000,"beds":2,"baths":1,"sqft":909,"dom":91,"img_src":"https://listing-images.homejunction.com/crmls/1169348988/photo_1.jpg","zillow_link":"https://www.zillow.com/homedetails/56674-Cone-Blvd-Landers-CA-92285/17507455_zpid/","fin":compute(265000,0,1710.48,79500.0,909)},
+    {"address":"5188 Godwin Rd, Twentynine Palms, CA 92277","region":"29 Palms","price":375000,"beds":2,"baths":2,"sqft":1524,"dom":5,"img_src":"photos/5188-godwin-rd.jpg","zillow_link":"https://www.zillow.com/homedetails/5188-Godwin-Rd-Twentynine-Palms-CA-92277/17506255_zpid/","fin":compute(375000,0,2337.34,112500,1524)},
 ]
 
 # ---- Comps: per-property Airbnb comps. Averages drive the property's revenue assumption. ----
